b7c8b026a729bebf22bde5a6b87efedcb2ca1dc8
corcor322 <corcor322@gmail.com>
2024-09-25 13:29:53 -0400

    Add ShoppingList to workbook-1

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args){
+        System.out.println("-Eggs");
+        System.out.println("-Cheese");
+        System.out.println("-Yogurt");
+        System.out.println("-Apples");
+        System.out.println("-Peppermint");
+        System.out.println("-Cat Food");
+        System.out.println("-Hot Paprika");
+        System.out.println("-Wide Egg Noodles");
+        System.out.println("-Cinnamon");
+        System.out.println("-Curry Bricks");
+    }
+}
