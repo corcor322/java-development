db92e106ddd9e60229275752ee2d1759d6625630
corcor322 <corcor322@gmail.com>
2024-10-08 12:32:47 -0400

    Completed workbook-2  FullNameParser

--- workbook-2/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
+++ workbook-2/FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -5,29 +5,45 @@ import java.util.Scanner;
 
 public class FullNameParser {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // ask the user for their full name
-        System.out.println("Please enter your full name");
-        String fullName = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
 
-        // ask break the full name up by spaces
-        // if allNames has an index of 2:
-        //  -set 0= firstname 1=middle name 2=lastname
-        //  -print firstname, print middle name, print last name
-        // else if allNames has an index of 1
-        //  -set 0= firstname 1=lastname
-        //  -print firstname, print middle name, print last name
-        String[] allNames = fullName.split(" ");
-        System.out.println(Arrays.toString(allNames));
+            // Ask for name
+            System.out.print("Please enter your name: ");
+            String input = scanner.nextLine();
 
-        if (allNames[] == 2); {
-            String firstName = allNames[0];
-            String middleName =
+            // Store the first, middle & and last names in variables
+            String first = "";
+            String middle = "";
+            String last = "";
+
+            // Find the first space
+            int firstSpaceIndex = input.indexOf(' ');
+
+            if (firstSpaceIndex == -1) {
+                first = input;
+                last = input;
+         /*   System.out.println("Name format is wrong");
+            return;*/
+            } else {
+                first = input.substring(0, firstSpaceIndex);
+
+                input = input.substring(firstSpaceIndex).trim();
+
+                int lastSpaceIndex = input.indexOf(' ');
+
+                if (lastSpaceIndex == -1) {
+                    last = input;
+                } else {
+                    middle = input.substring(0, lastSpaceIndex);
+                    last = input.substring(lastSpaceIndex + 1);
+                }
+            }
+
+            // Display divided name parts
+            System.out.println("First name: " + first);
+            System.out.println("Middle name: " + (middle.isEmpty() ? "(none)" : middle));
+            System.out.println("Last name: " + last);
+
+            scanner.close();
         }
-        // if there's three names, break into first, middle and last name
-        //if (allNames) {
-        // if there's two names break into first and last name
-
-        // if there's one name print "invalid structure"
-    }
 }
