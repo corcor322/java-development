f5feb36ba68eeb0f96dd675697cff82eee839141
corcor322 <corcor322@gmail.com>
2024-10-08 11:25:27 -0400

    Added workbook-3 exercises

---
+++ workbook-3/BedtimeStories/src/main/java/com/pluralsight/Stories.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Stories {
+    public static void main(String[] args) {
+        String[] files = {"goldilocks.txt", "hansel_and_gretel.txt", "mary_had_a_little_lamb.txt"};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which story would you like to read?");
+        System.out.println("Press '1' for 'Goldilocks and the Three Bears'");
+        System.out.println("Press '2' for 'Hansel and Gretel'");
+        System.out.println("Press '3' for 'Mary had a Little Lamb'");
+
+        int input = scanner.nextInt();
+
+        // Corrected the condition to use && for range checking
+        if (input >= 1 && input <= 3) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(files[input - 1])))) {
+                String storyLine;
+                // Read the file line by line
+                while ((storyLine = reader.readLine()) != null) {
+                    System.out.println(storyLine);
+                }
+            } catch (Exception exception) {
+                System.out.println("Sorry, there was an error reading the file.");
+            }
+        } else {
+            System.out.println("Sorry, that's not a valid choice. Please choose a number between 1 and 3.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ workbook-3/FamousQuotes/src/main/java/com/pluralsight/main.java
@@ -0,0 +1,36 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String quotes[] = new String[10];
+
+            quotes[0] = "You are not obligated to complete the work, but neither are you free to desist from it. -Pirkei Avot";
+            quotes[1] = "If I am not for myself, then who will be for me? If am am not for others, what am I? - Rabbi Hillel";
+            quotes[2] = "The superior man is distressed by the limitations of his ability; he is not distressed by the fact that men do not recognize the ability that he has. -Confucius";
+            quotes[3] = "When you feel like";
+            quotes[4] = "EVERYBODY DANCE NOW- Inumaki is a cursed speech user";
+            quotes[5] = "Stand proud, you are strong.";
+            quotes[6] = "Throughout Heaven and Earth, I alone am the honored one.";
+            quotes[7] = "Maven";
+            quotes[8] = "Maven";
+            quotes[9] = "Maven";
+
+
+            // Prompt user for number between 1-10
+            System.out.println("Pick a number between 1 and 10.");
+            int input = scanner.nextInt();
+
+            // Display quote corresponding to that number
+            System.out.println(quotes[input - 1]);
+        } catch (Exception outOfBounds) {
+            System.out.println("Hmm...that's not right.");
+            outOfBounds.printStackTrace();
+        }
+
+    }
+}
