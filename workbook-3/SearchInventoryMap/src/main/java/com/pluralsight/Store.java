1191d538c4abedb6517570e6a7e3563f47957530
corcor322 <corcor322@gmail.com>
2024-10-09 15:28:02 -0400

    Completed SearchInventoryMap Exercise workbook-3

---
+++ workbook-3/SearchInventoryMap/src/main/java/com/pluralsight/Store.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+//TODO: Add Bonus Loop
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Store {
+
+    // the key is the product id, the value is a product object
+    static HashMap<Integer, Product> inventory =
+            new HashMap<Integer, Product>();
+    public static void main(String[] args) {
+
+
+        // this method loads product objects into inventory
+        loadInventory();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What item # are you interested in? ");
+        int id = scanner.nextInt();
+
+        Product matchedProduct = inventory.get(id);
+
+        if (matchedProduct == null) {
+            System.out.println("We don't carry that product");
+            return;
+        }
+        System.out.printf("We carry %s and the price is $%.2f",
+                matchedProduct.getName(), matchedProduct.getPrice());
+    }
+    public static void loadInventory() {
+        inventory.put(1, new Product(1, "Soda", 1.29));
+        inventory.put(2, new Product(2, "Water", 1.49));
+        inventory.put(3, new Product(3, "Beer", 3.00));
+        inventory.put(4, new Product(4,"Wine", 6.99));
+        inventory.put(5, new Product(5,"Potato", 0.49));
+    }
+}
