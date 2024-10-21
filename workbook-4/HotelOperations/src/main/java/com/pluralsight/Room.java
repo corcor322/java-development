76322a28d411241a37bc493ae79207d2c9aa4c05
corcor322 <corcor322@gmail.com>
2024-10-21 14:35:29 -0400

    Added default values for booleans

--- workbook-4/HotelOperations/src/main/java/com/pluralsight/Room.java
+++ workbook-4/HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -7,13 +7,31 @@ public class Room {
     private boolean isDirty;
     private boolean isAvailable;
 
-    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty, boolean isAvailable) {
+    public Room(int numberOfBeds, double price, boolean isAvailable) {
         this.numberOfBeds = numberOfBeds;
         this.price = price;
-        this.isOccupied = isOccupied;
-        this.isDirty = isDirty;
+        this.isOccupied = false;
+        this.isDirty = false;
         this.isAvailable = isAvailable;
     }
 
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
 
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
